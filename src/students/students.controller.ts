0ce5142c56d54074caae7b32d152ba5747d720ec
Ivan Mendoza <ivan.mendoza@externos-cl.cencosud.com>
2022-12-08 22:39:48 -0400

    Agregnado docu

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Students')
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+  @Get()
+  @ApiOperation({ summary: 'Método para obtener todos los estudiantes.' })
+  getAll() {
+    return [];
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: 'Método para obtener los estudiantes por Id.' })
+  get(@Param('id') id: number) {
+    return `Students Id: ${id}`;
+  }
+}
--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Teachers')
 @Controller('teachers')
-export class TeachersController {}
+export class TeachersController {
+  @Get()
+  @ApiOperation({ summary: 'Método para obtener todos los docentes.' })
+  getAll() {
+    return [];
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: 'Método para obtener un solo docente por Id.' })
+  get(@Param('id') id: number) {
+    return `Teachers Id: ${id}`;
+  }
+}
