151c2970522434f4f2f65063199419ae3de4b523
Ivan Mendoza <ivan.mendoza@externos-cl.cencosud.com>
2022-12-09 00:43:54 -0400

    Agregando documentacion

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { StudentsService } from './services/students.service';
 
 @ApiTags('Students')
@@ -9,12 +14,26 @@ export class StudentsController {
 
   @Get()
   @ApiOperation({ summary: 'Método para obtener todos los estudiantes.' })
+  @ApiOkResponse({
+    isArray: true,
+    status: 200,
+    description: 'Devolverá un listado de estudiantes.',
+  })
   getAll() {
     return this.studentsService.getAll();
   }
 
   @Get('/:code')
   @ApiOperation({ summary: 'Método para obtener los estudiantes por Id.' })
+  @ApiOkResponse({
+    isArray: true,
+    status: 200,
+    description: 'Devolverá un estudiante filtrado por código',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Estudiante no encontrado en la lista.',
+  })
   get(@Param('code') code: string) {
     return this.studentsService.findByCode(code);
   }
--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { TeachersService } from './services/teachers.service';
 
 @ApiTags('Teachers')
@@ -9,12 +14,26 @@ export class TeachersController {
 
   @Get()
   @ApiOperation({ summary: 'Método para obtener todos los docentes.' })
+  @ApiOkResponse({
+    isArray: true,
+    status: 200,
+    description: 'Devolverá un listado de docentes.',
+  })
   getAll() {
     return this.teachersService.getAll();
   }
 
   @Get('/:code')
   @ApiOperation({ summary: 'Método para obtener un solo docente por Id.' })
+  @ApiOkResponse({
+    isArray: true,
+    status: 200,
+    description: 'Devolverá un Docente filtrado por código',
+  })
+  @ApiNotFoundResponse({
+    status: 404,
+    description: 'Docente no encontrado en la lista.',
+  })
   get(@Param('code') code: string) {
     return this.teachersService.findByCode(code);
   }
